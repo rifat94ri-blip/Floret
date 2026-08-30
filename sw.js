52c590bd89fcaeb73e2fd299eb15f85c839c267c
rifat94ri-blip <rifat94.ri@gmail.com>
2026-08-30 13:54:06 +0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE = 'rifat-business-manager-v1';
+const ASSETS = ['./', './index.html', './manifest.webmanifest'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => cached)));
+});
